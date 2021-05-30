837a4a18ee5cd8a36439daa65cbeb845ef77d811
Armen Babayan <armenbabayan1984@gmail.com>
2021-05-31 00:46:44 +0400

    fix cors error

--- index.js
+++ index.js
@@ -5,11 +5,31 @@ const io = require('socket.io')(server, {
     cors: { origin: "*"}
 })
 const path = require('path')
-
+const cors = require('cors')
 const port = process.env.port || 3001
 
+app.use(cors());
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
+// // cors //
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, Origin, X-Requested-With')
+//     next()
+//   })
+// /////////
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, PUT, OPTION"
+      )
+    next()
+  })
 
 app.set('view engine', 'ejs')
 
